7d69168e3d5a9ccef1aa40d4ddbaead2302191fb
Anki <ankhbayarbatsukh@gmail.com>
2018-07-25 02:08:53 +0200

    switching template

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "ns-app",
-    template: `<page-router-outlet></page-router-outlet>`
-    /*template:
+    // template: `<page-router-outlet></page-router-outlet>`
+    template:
     `
     <RadSideDrawer>
         <StackLayout tkDrawerContent background="grey">
@@ -13,7 +13,7 @@ import { Component } from "@angular/core";
             <page-router-outlet></page-router-outlet>
         </StackLayout>
     </RadSideDrawer>
-    `*/
+    `
 })
 
 export class AppComponent { }
